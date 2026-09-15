e848ce9a7a2d8c6b499acd414445339142119ad4
luismutton <lfmutton@gmail.com>
2026-09-15 19:00:21 -0300

    feat(BLUE): Creating quandoIniciado...() e deveBloquearAcesso...()

--- src/test/java/com/example/grupo_2_gameeducator/model/AlunoCursoTest.java
+++ src/test/java/com/example/grupo_2_gameeducator/model/AlunoCursoTest.java
@@ -6,4 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AlunoCursoTest {}
+class AlunoCursoTest {
+    @Test
+    void quandoIniciadoUmCurso() {
+        // DADO
+        var curso = new Curso();
+        var aluno = new Aluno(8.0);
+
+        // QUANDO
+        aluno.finalizarCurso(curso);
+
+        // ENTÃO
+        assertFalse(curso.isFinalizado());
+    }
+
+    @Test
+    void deveBloquearAcessoQuandoMensalidadeNaoForPaga() {
+        // DADO
+        var curso = new Curso();
+        var mensalidade = new Mensalidade(StatusMensalidade.PENDENTE);
+        var aluno = new Aluno(8.0, mensalidade);
+
+        // QUANDO
+        aluno.finalizarCurso(curso);
+
+        // ENTÃO
+        assertFalse(aluno.temAcessoAoCurso());
+        assertTrue(aluno.isPlataformaCongelada());
+    }
+}
