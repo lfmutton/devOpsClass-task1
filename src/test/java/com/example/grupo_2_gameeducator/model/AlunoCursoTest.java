6355893bc32902147035d6d9e8aa057ae5cbffee
luismutton <lfmutton@gmail.com>
2026-09-15 00:13:20 -0300

    Feat(Test): Creating AlunoCursoTest

---
+++ src/test/java/com/example/grupo_2_gameeducator/model/AlunoCursoTest.java
@@ -0,0 +1,9 @@
+package com.example.grupo_2_gameeducator.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AlunoCursoTest {}
