1a7b53622951bbfc17f102872b2f169350f7bc65
Will <williamrd252004@gmail.com>
2026-09-15 19:05:19 -0300

    Feat(GREEN): Creating deveBloquearAcessoQuandoMensalidadeNaoForPaga

--- src/test/java/com/example/grupo_2_gameeducator/model/AlunoCursoTest.java
+++ src/test/java/com/example/grupo_2_gameeducator/model/AlunoCursoTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AlunoCursoTest {
+
+    //Mute
     @Test
     void quandoIniciadoUmCurso() {
         // DADO
@@ -18,5 +20,21 @@ class AlunoCursoTest {
 
         // ENTÃO
         assertFalse(curso.isFinalizado());
+    }
+
+    //Will
+    @Test
+    void deveBloquearAcessoQuandoMensalidadeNaoForPaga() {
+        // DADO
+        var curso = new Curso();
+        var mensalidade = new Mensalidade(StatusMensalidade.PENDENTE);
+        var aluno = new Aluno(8.0, mensalidade);
+
+        // QUANDO
+        aluno.finalizarCurso(curso);
+
+        // ENTÃO
+        assertFalse(aluno.temAcessoAoCurso());
+        assertTrue(aluno.isPlataformaCongelada());
     }
 }
